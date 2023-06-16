8ac98a8bbbe2a5a03542a480e51f134b1f4fc53a
JUANNEITOR98 <jdgarcia3672@misena.edu.co>
2023-06-15 20:30:32 -0500

    Reestructuracion
    Reestructure de 0 todo el HTML, el css y el js

---
+++ Pagina completa/js/validation_empleados.js
@@ -0,0 +1,33 @@
+function validacion_empleados(){
+	var user=document.getElementById("usuarios").value;
+	var date=document.getElementById("date").value;
+	var doc=document.getElementById("typeDocument").value;
+	var ndoc=document.getElementById("ndocumento").value;
+	var email=document.getElementById("email").value;
+	var Apellido=document.getElementById("Apellido").value;
+	
+if (Apellido=="") {
+		alert("POR FAVOR REGISTRE SU APELLIDO");
+		return false;
+	}
+if (user=="") {
+	alert("POR FAVOR DIGITE EL USUARIO");
+	return false;
+}
+if (date==""){
+	alert("POR FAVOR DIGITE LA FECHA");
+	return false;
+}
+if (doc==""){
+	alert("POR FAVOR SELECCIONE EL TIPO DE DOCUMENTO");
+	return false;
+}
+if (ndoc==""){
+	alert("POR FAVOR DIGITE EL DOCUMENTOD");
+	return false;
+}
+if (email==""){
+	alert("POR FAVOR DIGITE EL EMAIL");
+	return false;
+}
+}
