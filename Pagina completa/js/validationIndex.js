932938f8cf6ed412b8ff27de5b99aad5fe43c375
JUANNEITOR98 <jdgarcia3672@misena.edu.co>
2023-06-16 16:47:59 -0500

    version  4.0
    intente conectar las paginas pero el profesor dice q toca despues de vacaciones

--- Pagina completa/js/validationIndex.js
+++ Pagina completa/js/validationIndex.js
@@ -6,10 +6,12 @@ function ValidationEvent() {
         
     if (user == 'admin') {
           
-        if ( pass == '12345' ) {            
-			alert("Usuario Logeado.....!");
+        if ( pass == '12345' ) {
+            alert("Usuario Logeado.....!");
             window.location.assign("http://127.0.0.1:5500/Pagina%20completa/tablas.html")
-        } 
+
+        }
+
         else {
             if (pass == '') {
                 alert("Campo PassWord VACIO.....!");
